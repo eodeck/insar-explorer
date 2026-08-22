567af96d68907174d9eb93926fde29f7920f0db8
mahmud1 <mahmud1@users.noreply.github.com>
2026-08-22 20:05:31 +0200

    bump version to 2.8.0

--- __init__.py
+++ __init__.py
@@ -23,8 +23,8 @@
  This script initializes the plugin, making it known to QGIS.
 """
 
-__version__ = "2.8.0dev"
-__date__ = "2026.07.29"
+__version__ = "2.8.0"
+__date__ = "2026.08.22"
 
 
 # noinspection PyPep8Naming
